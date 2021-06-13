c8344aee765132f24ee8c9b4977de86a0799c9c8
abcgroupuae <media@abcgroup.ae>
2021-06-13 17:52:24 +0400

    key removed

--- resources/views/globalpresence.blade.php
+++ resources/views/globalpresence.blade.php
@@ -91,8 +91,8 @@
 
 <!-- <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=5f2fc59bafe8e6083cb57bd1" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script> -->
   <!-- <script src="js/webflow.js" type="text/javascript"></script> -->
-  <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
-  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBoMMXN6s3a--R6cHM1ssTW5zPL83MhuAk&callback=initMap&libraries=&v=weekly" defer></script>
+  <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] google-key -  AIzaSyBoMMXN6s3a--R6cHM1ssTW5zPL83MhuAk&callback=initMap&libraries=&v=weekly -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=&callback=initMap&libraries=&v=weekly" defer></script>
   <script>
     "use strict";
     const iconbase='images/'
